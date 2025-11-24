4a7b8b6de1eb1aa09481733739412ef1c56ad1b5
vishwajeetkanchanwar2-dev <vishwajeetkanchanwar2@gmail.com>
2025-11-24 18:10:37 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://formlix-backend.onrender.com';
+const API_BASE_URL = 'https://formlix-backend.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
